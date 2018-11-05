f6cff5b014c6e53925ef863ddfe1dfc413eb6eb7
Emmanuel Araya Gamboa <emmanuelaraya114@gmail.com>
2018-11-04 21:42:35 -0600

    Arreglos a modulacion con Kf y Km, se redifinio el efecto del indice de mod

--- AM_modulation.m
+++ AM_modulation.m
@@ -6,35 +6,25 @@ clc
  
 fprintf('\n Modulaciones Analogicas\n');
 
-Fre = input('\nSe debe definir una frecuencia de muestreo: ');
+%Fre = input('\nSe debe definir una frecuencia de muestreo: ');
+Fre = 30000;
 t = 0:1/Fre:2;
-
-
-%Se le pregunta por los datos del la onda portadora
-fprintf('\n Se debe definir los parametros de la onda portadora\n ')
-F_c = input('Ingrese la frecuencia de la portadora: '); %Frecuencia de portadora
-A_c = input('Ingrese la amplitud de la portadora: '); %Amplitud de portadora
-Portadora = A_c*cos(2*pi*t*F_c); % Funcion de portadora
 
 
 %Se pregunta al usuario por los parametros de la onda de informacion 
 fprintf('\n Se debe definir los parametros de la onda de informacion\n ')
+Ai = input('\nIngrese la amplitud de la onda de informacion:'); 
+f_m = input('\nFrecuencia del la onda de informacion: ');
 fprintf('\nSeleccione entre las 4 funciones precargadas para determinar la onda de informacion:\n');
 fprintf('\n1-Senoidal \n2-Sawtooth \n3-Pulsos \n');
-
 Selec = input('\nSu eleccion es: ');
 
 if Selec ==1
-    f_m = input('Frecuencia: ');
-    m = cos(2*pi*f_m*t);
+    m = Ai.*cos(2*pi*f_m*t);            %Onda de informacion Senoidal
 elseif Selec ==2
-    f_m = input('Frecuencia del diente: ');
-    m = sawtooth(2*pi*f_m*t);
-    integral = trapz(t,m);
+    m = Ai.*sawtooth(2*pi*f_m*t);
 elseif Selec==3
-    f_m = input('Frecuencia del pulso: ');
-    m = square(2*pi*f_m*t);
-    integral = trapz(t,m);
+    m = Ai.*square(2*pi*f_m*t);
 end
 
 
@@ -46,45 +36,87 @@ Selec_2 = input('\nSeleccione la modulacion que desea aplicar: ');
 
 %Funciones para modulacion ------USAR ESTAS PARA GENERAR DATOS A PAGINA WEB
 
-In = input('Defina el indice de modulacion: ');
+%In = input('Defina el indice de modulacion: ');
 
 
 
 if Selec_2 == 1
-    Modulada = AM_LC(Portadora,m,In); %Funcion de modulacion AM-Long Carrier
+    In = input('Defina el indice de modulacion: ');
+    
+    %Se le pregunta por los datos del la onda portadora
+    fprintf('\n Se debe definir los parametros de la onda portadora\n ')
+    F_c = input('Ingrese la frecuencia de la portadora: '); %Frecuencia de portadora
+    A_c = Ai/In; %Se define la magnitud de la portadora en base a indice de modulacion y amplitud info
+    Portadora = A_c*cos(2*pi*t*F_c); % Funcion de portadora
+    
+    %Calculo de onda modulada
+    Modulada = AM_LC(Portadora,m,In); %Funcion de modulacion AM-Long Carrier - ver al final del script
+    
+    %Demodulacion y filtrado
     d = (A_c.*(1+In.*m).*cos(2.*pi.*F_c.*t)).*(A_c.*cos(2.*pi.*F_c.*t));
     butterw=conv(d,butter(25,F_c/(Fre/2)));
     rec=butterw(25:10000);
     ampl=rec*(((min(m)-max(m))/2)/((min(rec)-max(rec))/2));
     d2=min(ampl)+((max(ampl)-min(ampl))/2);
     d1=ampl-d2;
+    
 elseif Selec_2 == 2
+    In = input('Defina el indice de modulacion: ');
+    
+    %Se le pregunta por los datos del la onda portadora
+    fprintf('\n Se debe definir los parametros de la onda portadora\n ')
+    F_c = input('Ingrese la frecuencia de la portadora: '); %Frecuencia de portadora
+    A_c = Ai/In; %Se define la magnitud de la portadora en base a indice de modulacion y amplitud info
+    Portadora = A_c*cos(2*pi*t*F_c); % Funcion de portadora
+    
+    %Calculo de onda modulada
     Modulada =  AM_SC(Portadora,m,In);  %Funcion de modulacion AM-Supressed Carrier
+    
+    %Demod
     d = (A_c.*(In.*m).*cos(2.*pi.*F_c.*t)).*(A_c.*cos(2.*pi.*F_c.*t));
     d2 = conv(d,butter(20,F_c/(Fre/2)));
     rec = d2(25:10000);
     ampl=rec*(((min(m)-max(m))/2)/((min(rec)-max(rec))/2));
     d1=ampl;
+    
 elseif Selec_2 ==3
-    A_m = input('Defina amplitud del msj: ');
-    Modulada = PM(m,In,F_c,A_c,t,A_m); %Funcion de modulacion de fase
-    Kp= input('Defina sensibilidad del modulador de fase: '); 
-    phasedev=Kp.*A_m;
+    Kp= input('Defina sensibilidad del modulador de fase: ');
+    
+    %Se le pregunta por los datos del la onda portadora
+    fprintf('\n Se debe definir los parametros de la onda portadora\n ')
+    F_c = input('Ingrese la frecuencia de la portadora: '); %Frecuencia de portadora
+    A_c = input('Defina amplitud de la onda portadora: ');
+    Portadora = A_c*cos(2*pi*t*F_c); % Funcion de portador
+    
+    Modulada = PM(m,Kp,F_c,A_c,t); %Funcion de modulacion de fase
+    
+    %Demod
+    phasedev = Kp.*Ai;
     d = pmdemod(Modulada,F_c,Fre,phasedev);
     d2 = conv(d,butter(30,F_c/(Fre/2)));
     rec = d2(25:10000);
     ampl=rec*(((min(m)-max(m))/2)/((min(rec)-max(rec))/2));
     d1=ampl;
+    
 elseif Selec_2 ==4
-    A_m = input('Defina amplitud del msj: ');
-    Modulada = FM(m,In,F_c,A_c,t,A_m); %Funcion de modulacion de frecuencia
-    Kf= input('Defina sensibilidad del modulador de frecuencia: '); 
-    freqdev=Kf.*A_m;
+    Kf= input('Defina sensibilidad del modulador de frecuencia: ');
+    
+    %Se le pregunta por los datos del la onda portadora
+    fprintf('\n Se debe definir los parametros de la onda portadora\n ')
+    F_c = input('Ingrese la frecuencia de la portadora: '); %Frecuencia de portadora
+    A_c = input('Defina amplitud de la onda portadora: ');
+    Portadora = A_c*cos(2*pi*t*F_c); % Funcion de portador
+    
+    Modulada = FM(m,Kf,F_c,A_c,t); %Funcion de modulacion de frecuencia
+    
+    %Demod
+    freqdev=Kf.*Ai;
     d = fmdemod(Modulada,F_c,Fre,freqdev);
     d2 = conv(d,butter(30,F_c/(Fre/2)));
     rec = d2(25:10000);
     ampl=rec*(((min(m)-max(m))/2)/((min(rec)-max(rec))/2));
     d1=ampl;
+    
 end
 
 %Analisis Espectral de la senal
@@ -93,14 +125,14 @@ end
 %GRAFICAS
 %---------------------------------------------------------------------
 
-% Gráfica de portadora
+% GrÃ¡fica de portadora
 subplot(5,1,1); %grafica de portadora
 plot(t(1:F_c),Portadora(1:F_c)),xlabel('tiempo(s)'),ylabel('Amplitud (V)'); %grafica de portadora 
 grid on
 title('Onda Portadora');
 
 
-subplot(5,1,2);  %Señal de informacion
+subplot(5,1,2);  %SeÃ±al de informacion
 
 if Selec==1
     plot(t(1:F_c),m(1:F_c)),xlabel('tiempo(s)'),ylabel('Amplitud (V)');
@@ -118,7 +150,7 @@ end
 
 
 % Grafica de senal modulada
-subplot(5,1,3);  %Señal de informacion
+subplot(5,1,3);  %SeÃ±al de informacion
 plot(t(1:F_c),Modulada(1:F_c)),xlabel('tiempo(s)'),ylabel('Amplitud (V)');
 grid on
 title ('Onda Modulada');
@@ -132,7 +164,7 @@ grid on
 title ('Analisis Espectral');
 
 
-% Gráfica de onda demodulada
+% GrÃ¡fica de onda demodulada
 subplot(5,1,5), plot(t(1:F_c),d1(1:F_c)),xlabel('tiempo (s)'),ylabel('Demodulada');
 grid on
 title ('Onda Demodulada');
@@ -145,12 +177,12 @@ function x = AM_SC(Portadora,m,In)
     x = In.*m.*Portadora;
 end
 
-function x = PM(m,In,F_c,A_c,t,A_m)
-    x = A_c.*cos(2.*pi.*F_c.*t + (In*A_m).*m);
+function x = PM(m,Kp,F_c,A_c,t)
+    x = A_c.*cos(2.*pi.*F_c.*t + Kp.*m);
 end
 
-function x = FM(m,In,F_c,A_c,t,A_m)
-   x = A_c.*cos(2.*pi.*F_c.*t - (In/A_m).*m);
+function x = FM(m,Kf,F_c,A_c,t)
+   x = A_c.*cos(2.*pi.*F_c.*t + 2.*pi.*Kf.*m); %Falta definir integral
 end
 
 
